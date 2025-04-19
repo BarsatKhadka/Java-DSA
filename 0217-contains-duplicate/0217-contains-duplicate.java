768318dda9551c1e134e98c0a34ee181995db38f
barsatKhadka <khadkabarsat598@gmail.com>
2025-04-19 14:25:05 -0500

    0217-CONTAINS-DUPLICATE Stats: Time: 13 ms (43.86%), Space: 58.1 MB (48.81%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,23 +1,13 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-
-        Set<Integer> hashSet = new HashSet<>();
-
-        for(int i=0; i< nums.length; i++){
-
-            //if this returns false then num is already in set
-           boolean isAdded = hashSet.add(nums[i]);
-
-           //if the number is already in the set then return true (read the qsn carefully)
-           if(!isAdded){
-            return true;
-           }
-            
-
+        HashSet<Integer> set = new HashSet<>();
+        for(int i: nums){
+            if(set.contains(i)){
+                return true;
+            }
+            set.add(i);
         }
-
         return false;
-        
         
     }
 }
