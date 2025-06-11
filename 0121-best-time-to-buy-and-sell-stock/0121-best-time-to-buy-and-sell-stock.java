c15f3427d3e079def113d850cc1b301338e26e18
barsatKhadka <khadkabarsat598@gmail.com>
2025-06-11 17:37:53 -0500

    0121-BEST-TIME-TO-BUY-AND-SELL-STOCK Stats: Time: 1 ms (99.93%), Space: 61.5 MB (74.53%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,26 +1,20 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int profit = 0;
         int min = prices[0];
+        int profit = 0;
 
-        //so how i understand this problem is that i need to find the biggest difference , the biggest difference always occurs with the minimum number 
-        //and another rule is that i cannot revisit the previous numbers because i already bought that stock , so i need to look at the right hand side numbers to find the biggest difference.
-
-        for(int i=1; i<prices.length; i++){
-            if(prices[i] < min){
-                min = prices[i];
+        for(int price: prices){
+            if(price < min){
+                min = price;
             }
             else{
-                if(profit <  prices[i] -min){
-                    profit = prices[i]-min;
-                }
+                profit = Math.max(profit ,price - min);
             }
-
         }
+
+
+
         return profit;
-
-
-        
         
     }
 }
