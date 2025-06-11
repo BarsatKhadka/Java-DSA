c699180a06700a5c06eaf8f9580e50b4680d8025
barsatKhadka <khadkabarsat598@gmail.com>
2025-06-11 10:24:59 -0500

    0169-MAJORITY-ELEMENT Stats: Time: 4 ms (50.03%), Space: 53.1 MB (49.35%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
-        return nums[nums.length/2 ];
+        return nums[nums.length/2];
+        
     }
 }
