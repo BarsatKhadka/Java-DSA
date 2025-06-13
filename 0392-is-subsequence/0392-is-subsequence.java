f5455b6fbf1372441065e6a8b3eb3bc19c122dc8
barsatKhadka <khadkabarsat598@gmail.com>
2025-06-13 17:32:13 -0500

    0392-IS-SUBSEQUENCE Stats: Time: 2 ms (66.49%), Space: 41.4 MB (86.16%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,23 +1,16 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        //two pointers man
-        int Sptr = 0;
-        int Tptr = 0;
-        int approvedCount = 0;
+        int sptr = 0;
+        int tptr = 0;
+        while(sptr < s.length() && tptr < t.length()){
+            if(s.charAt(sptr) == t.charAt(tptr)){
+                sptr++;
+            }
+            tptr++;
 
-        
-        while(Tptr < t.length() && Sptr < s.length() ){
-            if(s.charAt(Sptr) == t.charAt(Tptr)){
-                Sptr +=1;
-                Tptr += 1;
-                approvedCount +=1;
-            }
-            else{
-                Tptr += 1;
-            }
         }
 
-        return approvedCount == s.length();
+        return sptr == s.length();
         
     }
 }
