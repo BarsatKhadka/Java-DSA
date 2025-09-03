af670979321427db2f909040655b3b532085d1cf
barsatKhadka <khadkabarsat598@gmail.com>
2025-09-03 08:23:39 -0500

    0283-MOVE-ZEROES Stats: Time: 2 ms (79.77%), Space: 46.1 MB (68.64%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int leftPtr = 0;
-        for(int rightPtr=0; rightPtr<nums.length ; rightPtr++){
+        for(int rightPtr = 0 ; rightPtr < nums.length; rightPtr++){
             if(nums[rightPtr] !=0){
                 int temp = nums[leftPtr];
                 nums[leftPtr] = nums[rightPtr];
                 nums[rightPtr] = temp;
-                leftPtr +=1;
+                leftPtr++;
             }
         }
-
         
     }
 }
