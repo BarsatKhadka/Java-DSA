d8b49e7a7ffe24fbf9f81accf76c7c1196018d78
barsatKhadka <khadkabarsat598@gmail.com>
2025-09-08 10:09:53 -0500

    0189-ROTATE-ARRAY Stats: Time: 0 ms (100%), Space: 57.3 MB (78.95%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -2,19 +2,21 @@ class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
         k = k%n;
-        reverseArr(nums, 0 , n-1);
-        reverseArr(nums , 0 , k-1);
-        reverseArr(nums , k, n-1);
+        //first full array is going to reverse [1,2,3,4] -> [4, 3,2,1] if k = 2, then what will you do?
+        // i would say [3,4,1,2] //reverse each parts
+        reverseArr(nums , 0 ,  n-1 );
+        reverseArr(nums , 0, k-1);
+        reverseArr(nums, k , n-1);
         
     }
 
-    public static void reverseArr(int[] arr , int start ,int end){
-        while(start <= end){
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-        start++;
-        end--;
+    private static void reverseArr(int[] nums, int start, int end){
+        while(start<=end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }
